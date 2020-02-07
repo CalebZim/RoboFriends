cbf3aecbef13a7f14f2ad18862b4eabced349de2
Caleb Zimmerman <54634315+CalebZim@users.noreply.github.com>
2020-02-07 12:06:39 -0500

    Updates

---
+++ precache-manifest.895780e28175309f05b73dd738d45a94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c23eec1f88c4156c53844f2c309539",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e297d795c4776c03db90",
+    "url": "/robofriends/static/css/main.feedfdb7.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "e297d795c4776c03db90",
+    "url": "/robofriends/static/js/main.8ab54946.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
